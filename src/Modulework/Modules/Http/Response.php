b5463b4d17cf6ba46a1e7b61d4a1c143a09b7a32
ChristianGaertner <christiangaertner.film@googlemail.com>
2013-07-28 19:19:17 +0200

    Adding Response class

---
+++ src/Modulework/Modules/Http/Response.php
@@ -0,0 +1,13 @@
+<?php namespace Modulework\Modules\Http;
+/*
+ * (c) Christian Gärtner <christiangaertner.film@googlemail.com>
+ * This file is part of the Modulework Framework
+ * License: View distributed LICENSE file
+ */
+
+/**
+* Response
+* This class should represent the HTTP response,
+* done by the application.
+*/
+class Response { }
