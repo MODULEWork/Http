a882d624c957939657249a22215f1f4577fd28c2
ChristianGaertner <christiangaertner.film@googlemail.com>
2013-07-28 19:14:51 +0200

    Fixing gh-push script

--- build/scripts/push_to_gh_pages.sh
+++ build/scripts/push_to_gh_pages.sh
@@ -11,6 +11,7 @@ if [ "$POST_BUILD" == "true" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 	# copy data we're interested in to other place
 	cp -Rv build/result/coverage $HOME/build/coverage
 	cp -Rv build/result/docs $HOME/build/docs
+	cp -Rv build/result/phploc.txt $HOME/build/phploc.txt
 	# go to home and setup git
 	cd $HOME
 	git config --global user.email "travis@travis-ci.org"
