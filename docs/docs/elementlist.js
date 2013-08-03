0e3a52784b3b6a70ba295295682cba3ef35694be
ChristianGaertner <christiangaertner.film@googlemail.com>
2013-08-03 16:13:08 +0200

    Clean up

--- docs/docs/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Countable"],["c","DateTime"],["c","IteratorAggregate"],["c","Modulework\\Modules\\Http\\Cookie"],["c","Modulework\\Modules\\Http\\Request"],["c","Modulework\\Modules\\Http\\Response"],["c","Modulework\\Modules\\Http\\Utilities\\ArrayCase"],["c","Modulework\\Modules\\Http\\Utilities\\ArrayCaseInterface"],["c","Modulework\\Modules\\Http\\Utilities\\FileCase"],["c","Modulework\\Modules\\Http\\Utilities\\HeaderCase"],["c","Modulework\\Modules\\Http\\Utilities\\HeaderWrapper"],["c","Modulework\\Modules\\Http\\Utilities\\HeaderWrapperInterface"],["c","Modulework\\Modules\\Http\\Utilities\\ServerCase"],["c","Traversable"]];
