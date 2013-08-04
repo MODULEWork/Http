934b6a0df5815c2750a730f95a4660a78942ebb1
ChristianGaertner <christiangaertner.film@googlemail.com>
2013-08-04 17:50:06 +0200

    Added first tests for the RedirectResponse

---
+++ tests/Modulework/Modules/Http/RedirectResponseTest.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * (c) Christian Gärtner <christiangaertner.film@googlemail.com>
+ * This file is part of the Modulework Framework Tests
+ * License: View distributed LICENSE file
+ *
+ * 
+ * This file is meant to be used in PHPUnit Tests
+ */
+
+use Modulework\Modules\Http\RedirectResponse;
+
+/**
+* PHPUnit Test
+*/
+class RedirectResponseTest extends PHPUnit_Framework_TestCase
+{
+	public function testMake()
+	{
+		$response = RedirectResponse::make('http://foo.bar');
+		$this->assertInstanceOf('Modulework\Modules\Http\RedirectResponse', $response);
+	}
+
+	public function testConstruct()
+	{
+		$response = new RedirectResponse('http://foo.bar');
+		$this->assertEquals('http://foo.bar', $response->getUrl());
+		$this->assertEquals(302, $response->getStatusCode());
+	}
+
+	public function testConstructExecption()
+	{
+		// To be written
+		$this->assertTrue(true);
+	}
+
+}
